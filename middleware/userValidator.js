1fd77cda0fb093ee1cd25f4b923b88ebe45219b6
amanuel tegegne <amanuel0924@gmail.com>
2024-02-11 15:52:12 +0300

    paswordvalidation added

--- middleware/userValidator.js
+++ middleware/userValidator.js
@@ -3,8 +3,15 @@ import joi from "joi"
 export const userValid = (req, res, next) => {
   const Schema = joi.object({
     username: joi.string().required(),
-    email: joi.string().required(),
-    password: joi.string().required(),
+    email: joi.string().email({ minDomainSegments: 2 }).required(),
+    password: joi
+      .string()
+      .min(8)
+      .max(30)
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/
+      )
+      .required(),
     role: joi.string().required(),
   })
   const { err } = Schema.validate(req.body, { abortEarly: false })
